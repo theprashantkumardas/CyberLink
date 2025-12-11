2da954f89f043764456d1bbd4cf5e07dca3875c1
Babu Mosai <prashantdas336@gmail.com>
2025-12-12 02:12:30 +0530

    added cors

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const API_URL = 'https://cyberlink-mp94.onrender.com';
+
+// AUTO-DETECT ENVIRONMENT
+const API_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5000' 
+  : 'https://cyberlink-mp94.onrender.com'; // Your BACKEND URL
 
 export const api = axios.create({
   baseURL: API_URL,
 });
 
 export const socket = io(API_URL, { autoConnect: false });
+
+
 
 export const authAPI = {
   login: (credentials) => api.post('/auth/login', credentials),
